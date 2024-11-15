c1f5035b894b36b4a215a899327842e9c7ebb78a
mingnorov <zovlonmingnorov@gmail.com>
2024-11-15 14:15:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let user = prompt ('ismingizni kiriting');
+alert (user);
+console.log (user);
+let familiya = prompt ('familiyangizni kiriting');
+alert (familiya);
+console.log (familiya);
+let age = prompt ('yoshingizni kiriting');
+alert (age);
+console.log (age);
+let fan = prompt ('yoqtirgan faningizni yozing');
+alert (fan);
+console.log (fan);
+let locotion = prompt ('yashaydigan tumaningizni kiriting');
+alert (locotion);
+console.log (locotion);
+alert    ('malumotingizni kiritganingoiz uchiun tashakkur');
